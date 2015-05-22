893461a12dc81b7de62ae3cbe50c7a67ecad1fb7
Peter Mottram <peter@sysnix.com>
2015-05-22 15:50:09 +0000

    Import of SYSPETE/DBIx-Class-Schema-Versioned-Inline-0.203.tar.gz from CPAN.
    gitpan-cpan-distribution: DBIx-Class-Schema-Versioned-Inline
    gitpan-cpan-version:      0.203
    gitpan-cpan-path:         SYSPETE/DBIx-Class-Schema-Versioned-Inline-0.203.tar.gz
    gitpan-cpan-author:       SYSPETE
    gitpan-cpan-maturity:     released

--- t/lib/Role/SQLite.pm
+++ t/lib/Role/SQLite.pm
@@ -1,7 +1,8 @@
 package Role::SQLite;
 
 use Class::Load qw(try_load_class);
-use File::Temp;
+use File::Spec;
+use File::Temp qw(tempfile);;
 
 use Test::Roo::Role;
 with 'Role::Database';
@@ -10,17 +11,25 @@ sub BUILD {
     try_load_class("DBD::SQLite") or plan skip_all => "DBD::SQLite required";
 }
 
-my $fh = File::Temp->new( TEMPLATE => 'upgrade_test_XXXXX', EXLOCK => 0 );
-my $dbfile = $fh->filename;
+my ( $fh, $dbfile ) = tempfile(
+    TEMPLATE => 'upgrade_test_XXXXX',
+    EXLOCK   => 0,
+    TMPDIR   => File::Spec->tmpdir
+);
 
+before clear_database => sub {
+    unlink($dbfile)
+      # can't unlink under Windows so on failure grab new tmpfile
+      # all our tempfiles will disappear when test exits
+      or ( $fh, $dbfile ) = tempfile(
+        TEMPLATE => 'upgrade_test_XXXXX',
+        EXLOCK   => 0,
+        TMPDIR   => File::Spec->tmpdir
+      );
+};
 after teardown => sub {
     shift->clear_database;
 };
-
-sub clear_database {
-    close($fh);
-    unlink($dbfile) or diag "Could not unlink $dbfile: $!";
-}
 
 sub _build_database {
 
@@ -35,7 +44,9 @@ sub _build_dbd_version {
 sub connect_info {
     my $self = shift;
 
-    return ( "dbi:SQLite:dbname=$dbfile", undef, undef,
+    return (
+        "dbi:SQLite:dbname=$dbfile",
+        undef, undef,
         {
             sqlite_unicode  => 1,
             on_connect_call => 'use_foreign_keys',
